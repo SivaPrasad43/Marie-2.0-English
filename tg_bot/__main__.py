1273f2efe18c789df0a65cedc702e54fa6351c1f
SivaPrasad43 <39110552+SivaPrasad43@users.noreply.github.com>
2020-11-06 21:08:50 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}!
+hello {}, my name is {}!
 
 im a group manager bot maintained by  [this person](tg://user?id={}).
 
